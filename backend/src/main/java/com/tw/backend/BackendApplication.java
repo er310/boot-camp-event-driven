e76192f9481e4ace0e7e68dc2158f18dbcceb8a1
rashdul.ehasan <rashdul.ehasan@thoughtworks.com>
2020-04-19 14:28:42 +0100

    backend service added

--- backend/src/main/java/com/tw/backend/BackendApplication.java
+++ backend/src/main/java/com/tw/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.tw.api;
+package com.tw.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
--- backend/src/test/java/com/tw/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/tw/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tw.api;
+package com.tw.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
